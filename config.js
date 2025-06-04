a8c6afe17f8a303ee515d3866f99abe50cfa547d
jahseh16 <148182550+jahseh16@users.noreply.github.com>
2025-06-03 23:58:07 -0500

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-import {watchFile, unwatchFile} from 'fs';
+import { watchFile, unwatchFile } from 'fs';
 import chalk from 'chalk';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import fs from 'fs'; 
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
@@ -9,90 +9,113 @@ import moment from 'moment-timezone';
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumber = '' //Ejemplo: 525218138672
+// BETA: Opcional, número del bot directamente
+global.botNumber = ''; // Ejemplo: 525218138672
 
-//──ׄ✞ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
+//──ׄ✞ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
 global.owner = [
   ['51935040872', '🜲 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 👻', true],
   ['573228284824']
 ];
 
-//─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
-
-global.mods = []
-global.suittag = ['51935040872'] 
-global.prems = []
+global.mods = [];
+global.suittag = ['51935040872'];
+global.prems = [];
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
-global.libreria = 'Baileys'
-global.baileys = 'V 6.7.8'
-global.vs = '2.0.0'
-global.languaje = 'Español'
-global.nameqr = 'Sakura-Bot-MD'
-global.sessions = 'blackSession'
-global.jadi = 'blackJadiBot'
-global.blackJadibts = true
+global.libreria = 'Baileys';
+global.baileys = 'V 6.7.8';
+global.vs = '2.0.0';
+global.languaje = 'Español';
+global.nameqr = 'Sakura-Bot-MD';
+global.sessions = 'blackSession';
+global.jadi = 'blackJadiBot';
+global.blackJadibts = true;
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
 global.packsticker = `♾ ━━━━━━━━
-├ ɓσƭ:
-├ ρяοριєταяιο:
-├ ƒєϲнα ∂є ϲяєαϲιόи:
-├ нοяα:
+├ ɓσƭ: Sakura-Bot
+├ ρяοριєταяιο: JAHSEH
+├ ƒєϲнα ∂є ϲяєαϲιόи: 04/06/2025
+├ нοяα: ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}
 ♾━━━━━━━━`;
-global.packname = *Sakura* ☘
-global.author = `♾━━━━━━━━\n⇝͟͞SAKURA ☘͟ ⋆\n⇝ ۵-̱̅ⁱ𝐓𝐇𝐄 JAHSEH-͞ˍ\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} \n♾━━━━━━━━\n\n\n\nѕτιϲκєя ϐγ: ৎSakura ☘͟ `;
+
+global.packname = 'Sakura ☘';
+global.author = `♾━━━━━━━━
+⇝͟͞SAKURA ☘͟ ⋆
+⇝ ۵-̱̅ⁱ𝐓𝐇𝐄 JAHSEH-͞ˍ
+⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}
+⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} 
+♾━━━━━━━━
+
+ѕτιϲκєя ϐγ: ৎSakura ☘͟ `;
+
 global.wm = 'Sakura ☘';
 global.titulowm = 'Sakura ☘';
-global.igfg = 'ᥫ𝐓𝐇𝐄 JAHSEH'
-global.botname = 'Sakura ☘'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ the Legends ⚡'
-global.textbot = 'SAKURA  : 𝐓𝐇𝐄 JAHSEH '
+global.igfg = 'ᥫ𝐓𝐇𝐄 JAHSEH';
+global.botname = 'Sakura ☘';
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ the Legends ⚡';
+global.textbot = 'SAKURA  : 𝐓𝐇𝐄 JAHSEH ';
 global.gt = '͟͞SAkura 𝕮𝖑𝖔𝖛𝖊𝖗 ☘͟͞';
-global.namechannel = 'SAKURA / 𝐓𝐇𝐄 JAHSEH'
+global.namechannel = 'SAKURA / 𝐓𝐇𝐄 JAHSEH';
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
-global.moneda = 'monedas'
+global.moneda = 'monedas';
 
-//• ↳ ◜𝑳𝑰𝑵𝑲𝑺  𝐓𝐇𝐄 𝐋𝐄𝐆𝐄𝐍𝐃𝐒 ™◞ • 🌿
-global.gp4 = 'https://chat.whatsapp.com/KpY3oQQVeCMJwfKNEmJHR8' //Grupo Oficial De black clover 
-global.gp1 = 'https://chat.whatsapp.com/IfV1FdKBdCuFeZitzNKZnb' //Grupo 2
-global.gp2 = 'https://chat.whatsapp.com/KpY3oQQVeCMJwfKNEmJHR8'
-global.channel = 'https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x' //Canal Oficial
-global.channel2 = 'https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x' //Canal test 
-global.yt = 'https://www.youtube.com/@El_JAHSEH17' //Canal De Youtube
-global.md = 'https://github.com/jahseh16' //Github Oficial
-global.correo = ''
-global.cn ='https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x';
+global.gp4 = 'https://chat.whatsapp.com/KpY3oQQVeCMJwfKNEmJHR8';
+global.gp1 = 'https://chat.whatsapp.com/IfV1FdKBdCuFeZitzNKZnb';
+global.gp2 = 'https://chat.whatsapp.com/KpY3oQQVeCMJwfKNEmJHR8';
+global.channel = 'https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x';
+global.channel2 = 'https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x';
+global.yt = 'https://www.youtube.com/@El_JAHSEH17';
+global.md = 'https://github.com/jahseh16';
+global.correo = '';
+global.cn = 'https://whatsapp.com/channel/0029Va25HLl9WtBvS2wfZ32x';
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
-global.estilo = { key: {  fromMe: false, participant: 0@s.whatsapp.net, ...(false ? { remoteJid: "51935040872-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = {
+  key: {
+    fromMe: false,
+    participant: '0@s.whatsapp.net',
+    ...(false ? { remoteJid: '51935040872-1625305606@g.us' } : {})
+  },
+  message: {
+    orderMessage: {
+      itemCount: -999999,
+      status: 1,
+      surface: 1,
+      message: global.packname,
+      orderTitle: 'Bang',
+      thumbnail: global.catalogo,
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+};
+
 global.ch = {
-ch1: '120363307694217288@newsletter',
-}
-global.multiplier = 70
+  ch1: '120363307694217288@newsletter',
+};
+global.multiplier = 70;
 
 //─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment   
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
 
-//─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─
+//─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─✞─ׄ─ׅ─ׄ─
 
-let file = fileURLToPath(import.meta.url)
+let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(${file}?update=${Date.now()})
-})
+  unwatchFile(file);
+  console.log(chalk.redBright("Update 'config.js'"));
+  import(${file}?update=${Date.now()});
+});
